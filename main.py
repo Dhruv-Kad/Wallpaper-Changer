1e3af417cf799937332650c7d4c60e1067d1143c
dhruv <kadamdhruv98@gmail.com>
2022-08-07 17:03:50 -0500

    Preparation for adding gui, changed main.py, Old main.py is in backups folder in case of catostrophic failure

---
+++ main.py
@@ -0,0 +1,62 @@
+from wallchanger import changer
+from currentime import fulltime
+from timer import main
+import random
+import time
+from colorama import Fore
+# fulltime() - Gets current time
+# changer() - Wallpaper changer
+# main() - Optional timer
+
+def Automatic(x):
+    raw0 = random.randint(600, 900)
+    min0 = int(raw0/60)
+    str0 = str(min0)
+    print(str0 + " minute cycle ")
+    while x == true:
+        changer()
+        time.sleep(raw0)
+
+def Manual():
+    changer()
+    timer()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#manual = input("Do you want to manually or automatically choose the wallpaper uptime? (A/M): ")
+#convman = manual.upper()
+#while 1 == 1:
+#    fulltime()
+#    if convman == 'M':
+#        changer()
+#        main()
+#    elif convman == 'A':
+#        raw0 = random.randint(600, 900)
+#        min0 = int(raw0/60)
+#        str0 = str(min0)
+#        print(str0 + " minute cycle ")
+#        changer()
+#        time.sleep(raw0)
+#    else:
+#        print( Fore.RED + "Please input yes or no")
+#        break
+
+
+
