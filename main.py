4496016d0007e911d707dabc0ec81188a1eb21f4
dhruv <kadamdhruv98@gmail.com>
2022-08-08 15:27:37 -0500

    added art and decided there is no point in a gui

--- main.py
+++ main.py
@@ -4,10 +4,12 @@ from timer import main
 import random
 import time
 from colorama import Fore
+from art import randascii
 # fulltime() - Gets current time
 # changer() - Wallpaper changer
 # main() - Optional timer
-
+# randascii() - Generates rand ascii art image
+randascii()
 def Automatic(x):
     raw0 = random.randint(600, 900)
     min0 = int(raw0/60)
@@ -21,42 +23,24 @@ def Manual():
     changer()
     timer()
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#manual = input("Do you want to manually or automatically choose the wallpaper uptime? (A/M): ")
-#convman = manual.upper()
-#while 1 == 1:
-#    fulltime()
-#    if convman == 'M':
-#        changer()
-#        main()
-#    elif convman == 'A':
-#        raw0 = random.randint(600, 900)
-#        min0 = int(raw0/60)
-#        str0 = str(min0)
-#        print(str0 + " minute cycle ")
-#        changer()
-#        time.sleep(raw0)
-#    else:
-#        print( Fore.RED + "Please input yes or no")
-#        break
+def Terminal():
+    manual = input("Do you want to manually or automatically choose the wallpaper uptime? (A/M): ")
+    convman = manual.upper()
+    while 1 == 1:
+        fulltime()
+        if convman == 'M':
+            changer()
+            main()
+        elif convman == 'A':
+            raw0 = random.randint(600, 900)
+            min0 = int(raw0/60)
+            str0 = str(min0)
+            print(str0 + " minute cycle ")
+            changer()
+            time.sleep(raw0)
+        else:
+            print( Fore.RED + "Please input yes or no")
+            break
 
 
 
